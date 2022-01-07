769901f285cc4ab3fafd6337aeee4897d331522c
grefle <timesetday61@gmail.com>
2022-01-08 01:15:38 +0200

    eslint added

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-export const PORT = 3000
-export const TOKEN = '5078057001:AAFm5gsehtDRbQbxb1uHcZTB-V_mXMCjyv8'
+export const TOKEN = "5078057001:AAFm5gsehtDRbQbxb1uHcZTB-V_mXMCjyv8"
 
 
--- index.js
+++ index.js
@@ -1,39 +1,39 @@
-'use strict';
+"use strict";
 
 import fetch from "node-fetch";
-import {TOKEN} from  './config.js'
-import {Telegraf} from 'telegraf'
-const apikey = '4c62d4734ce269a5daa0ab6aa0dd4a82';
+import {TOKEN} from  "./config.js"
+import {Telegraf} from "telegraf"
+const apikey = "4c62d4734ce269a5daa0ab6aa0dd4a82";
 let url = (city) =>
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
 
 const bot = new Telegraf(TOKEN)
 
 bot.start(ctx => {
-    ctx.reply('Доброго времени суток!')
+    ctx.reply("Доброго времени суток!")
 })
 
-bot.on('voice', ctx => {
-    ctx.reply('Я не знаю что на это ответить...')
+bot.on("voice", ctx => {
+    ctx.reply("Я не знаю что на это ответить...")
 })
 
-bot.on('sticker', ctx => {
-    ctx.reply('Хороший стикер, жаль я его не вижу')
+bot.on("sticker", ctx => {
+    ctx.reply("Хороший стикер, жаль я его не вижу")
 })
 
-bot.on('edited_message', ctx => {
-    ctx.reply('Зачем вы изменили сообщение?')
+bot.on("edited_message", ctx => {
+    ctx.reply("Зачем вы изменили сообщение?")
 })
 
-bot.hears('/help',ctx => {
-    ctx.reply('Чтобы начать пользоваться ботом достаточно просто ввести город, погода в котором вас интересует')
+bot.hears("/help",ctx => {
+    ctx.reply("Чтобы начать пользоваться ботом достаточно просто ввести город, погода в котором вас интересует")
 })
 
-bot.hears('/catfact',async ctx => {
+bot.hears("/catfact",async ctx => {
         const CatFact = async ( )=> {
-            ctx.reply('Простите, факты только на английском языке🐱');
+            ctx.reply("Простите, факты только на английском языке🐱");
             try {
-                await fetch('https://catfact.ninja/fact')
+                await fetch("https://catfact.ninja/fact")
                     .then(function (resp) {
                         return resp.json()
                     })
@@ -42,16 +42,16 @@ bot.hears('/catfact',async ctx => {
                     })
             }
             catch(e){
-                ctx.reply('Простите, я сломался, мой создатель не очень смышленый')
+                ctx.reply("Простите, я сломался, мой создатель не очень смышленый")
             }
         }
-    CatFact ( )
+    await CatFact ( )
     })
 
 bot.launch()
-console.log(`My Bot started`)
+console.log("My Bot started")
 
-bot.on('text', async (ctx) => {
+bot.on("text", async (ctx) => {
 
     const ToCeliac = (degree) => (degree - 273).toFixed(2);
 
@@ -67,7 +67,7 @@ bot.on('text', async (ctx) => {
 
     const FindDataForWeather = async ( city)=> {
         try {
-            await fetch(url(city), {origin: 'cors'})
+            await fetch(url(city))
                 .then(function (resp) {
                     return resp.json()
                 }) // превращает данные в json
@@ -76,9 +76,9 @@ bot.on('text', async (ctx) => {
                 })
         }
         catch(e){
-                ctx.reply('Такого города не существует, повторите пожалуйста запрос')
+                ctx.reply("Такого города не существует, повторите пожалуйста запрос")
             }
     }
     const city = ctx.message.text
-    FindDataForWeather( city )
+    await FindDataForWeather( city )
 })
